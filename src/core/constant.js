336fea968ad2b61698aaa749ca9d409785e7d776
lucdotdev <freshtutoriel@gmail.com>
2021-11-01 13:25:47 +0200

    commit

---
+++ src/core/constant.js
@@ -0,0 +1,21 @@
+const sid_tt = ["sid_tt=a251208807274e8c393e4ef5d43c3bae;"];
+const intro = "./assets/intro.mp4";
+const outro = "./assets/outro.mp4";
+const yt_email = "";
+const yt_password = "";
+function description(name ,number, video) {
+    return `
+Best TikTok of ${name} Compilation #${number} 😎😋😎
+Watching you will laugh all day 😝😝😜
+
+🤣 Check out more great Tik Tok videos: ${video}
+🔔 Subscribe for more of the funniest & best new TikTok videos: 
+https://www.youtube.com/c/TikiToku
+ 
+Contact: tikitokuvids@gmail.com
+
+Thank you guys so much for all the support on TikiToku!! We love 
+making these Cool Compilations for you!
+
+`};
+module.exports = { sid_tt, intro, outro, description,yt_email, yt_password };
